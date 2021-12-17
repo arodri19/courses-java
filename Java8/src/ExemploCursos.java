b867c5e7db13c44ce74b6f85fd56268c1e2ba269
arodri19 <andre.rodrigues19@outlook.com.br>
2021-12-17 20:06:55 -0300

    termino curso novidades java 8

---
+++ Java8/src/ExemploCursos.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+}
+
+public class ExemploCursos {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		
+		cursos.forEach(c -> System.out.println(c.getNome()));
+		
+		cursos.stream().filter(c -> c.getAlunos() >= 100);
+		
+		cursos.forEach(c -> System.out.println(c.getNome()));
+		
+		cursos.stream().filter(c -> c.getAlunos() >= 100).forEach(c-> System.out.println(c.getNome()));
+		
+		cursos.stream().filter(c -> c.getAlunos() >= 100).map(c->c.getAlunos()).forEach(c-> System.out.println(c));
+		
+		Optional<Curso> optionalCurso = cursos.stream().filter(c-> c.getAlunos() >= 100).findAny();
+		optionalCurso.get();
+		Curso curso = optionalCurso.orElse(null);
+		System.out.println(curso.getNome());
+		optionalCurso.ifPresent(c->System.out.println(c.getNome()));
+		
+		
+		//cursos = cursos.stream().filter(c-> c.getAlunos() >= 100).collect(Collectors.toList());
+		
+		cursos.stream()
+				.filter(c-> c.getAlunos() >= 100)
+				.collect(Collectors.toMap(
+						c-> c.getNome(),
+						c->c.getAlunos()))
+				.forEach((nome, alunos)-> System.out.println(nome + " tem " + alunos + " alunos"));
+	}
+}
