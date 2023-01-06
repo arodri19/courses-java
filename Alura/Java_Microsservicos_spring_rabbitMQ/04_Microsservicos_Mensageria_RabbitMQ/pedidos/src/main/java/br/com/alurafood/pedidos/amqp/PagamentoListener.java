02e200d0efbe827369390743c85eeeec15225491
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2023-01-05 22:00:16 -0300

    feat: terminando aula03 curso_microsservicos_rabbitMQ

--- Alura/Java_Microsservicos_spring_rabbitMQ/04_Microsservicos_Mensageria_RabbitMQ/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ Alura/Java_Microsservicos_spring_rabbitMQ/04_Microsservicos_Mensageria_RabbitMQ/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,14 +1,20 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoListener {
 
-    @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMesagem(Message mensagem) {
-        System.out.println("Recebi a mensagem " + mensagem.toString());
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
+    public void recebeMesagem(PagamentoDto pagamento) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$ %s
+                Status: %s
+                """.formatted(pagamento.getId(), pagamento.getPedidoId(), pagamento.getValor(), pagamento.getStatus());
+        System.out.println("Recebi a mensagem " + mensagem);
     }
 }
